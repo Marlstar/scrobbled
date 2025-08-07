51f5ec51e6760033d9e80153f6c8525f91510f15
Marley Reeves <marley@marlstar.com>
2025-08-07 15:09:52 +1000

    refactor(auth): implement `Display` for `OAuthToken`

--- src/auth/mod.rs
+++ src/auth/mod.rs
@@ -11,3 +11,8 @@ pub async fn get_token() -> Result<OAuthToken, OAuthError> {
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct OAuthToken(pub String);
+impl std::fmt::Display for OAuthToken {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
