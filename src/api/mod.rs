a57ee1a35645d6f235a35ec871caf9d31e5b8943
Marley Reeves <marley@marlstar.com>
2025-08-10 08:02:20 +1000

    refactor(api): improve `run!` to gracefully handle `reqwest::Error`s

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -10,26 +10,29 @@ pub type APIResult<T> = Result<T, APIError>;
 pub enum APIError {
     ResponseNotOK(u16),
     Deserialisation(serde_json::Error),
+    Reqwest(reqwest::Error),
 }
 
 #[macro_export]
 macro_rules! run {
-    ($namespace:ident, $method:ident, $outtype:ident, $reqtype:ident, $ws:expr $(,$arg:expr)*) => { async {
-        let req = format!(
-            "{}?{}&format=json",
-            $crate::api::API_URL,
-            $crate::api::$namespace::$method::$method($($arg),+)
-        );
+    ($namespace:ident, $method:ident, $outtype:ident, $reqtype:ident, $ws:expr $(,$arg:expr)*) => {{
+        let __inner__ = async || -> $crate::api::APIResult<$crate::api::$namespace::$method::$outtype> {
+            let req = format!(
+                "{}?{}&format=json",
+                $crate::api::API_URL,
+                $crate::api::$namespace::$method::$method($($arg),+)
+            );
 
-        // TODO: don't unwrap here
-        let result = $ws.execute($ws.$reqtype(req).build().unwrap()).await.unwrap();
-        let status_code = result.status().as_u16();
-        let out: $crate::api::APIResult<$crate::api::$namespace::$method::$outtype> = if status_code == 200 {
-            serde_json::from_str(&result.text().await.unwrap())
-                .map_err($crate::api::APIError::Deserialisation)
-        } else {
-            Err($crate::api::APIError::ResponseNotOK(status_code))
+            let result = $ws.execute($ws.$reqtype(req).build()?).await?;
+            let status_code = result.status().as_u16();
+            let out: $crate::api::APIResult<$crate::api::$namespace::$method::$outtype> = if status_code == 200 {
+                serde_json::from_str(&result.text().await?)
+                    .map_err($crate::api::APIError::Deserialisation)
+            } else {
+                Err($crate::api::APIError::ResponseNotOK(status_code))
+            };
+            out
         };
-        out
-    }}
-} pub use run;
+        __inner__()
+    }
+}} pub use run;
